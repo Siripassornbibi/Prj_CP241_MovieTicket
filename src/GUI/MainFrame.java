f5edde5c82de4a3144706045848723e1fa7debf6
Pinthucha <Pinthucha.ruckpintuwat@g.swu.ac.th>
2023-11-24 00:25:17 +0700

    update

--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -68,18 +68,6 @@ public class MainFrame extends JFrame{
         JPanel1.setPreferredSize(preferredSize);
         JPanelCenter.add(JPanel1);
 
-        //เพิ่มเก้าอี้ตัวที่1
-        ImageIcon imageIcon1 = new ImageIcon("src/picture/PinkChair.png");
-        JLabel imageLabel1 = new JLabel(imageIcon1);
-        JPanel1.add(imageLabel1);
-
-        //เพิ่มเก้าอี้ตัวที่2
-        ImageIcon imageIcon2 = new ImageIcon("src/picture/AnotherImage.png");
-        JLabel imageLabel2 = new JLabel(imageIcon2);
-        JPanel1.add(imageLabel2);
-
-
-
         JPanel JPanel2 = new JPanel();
         JPanel2.setBackground(Color.GRAY);
         Dimension preferredSize2 = new Dimension(350, 500); // กำหนดขนาดความกว้างและความสูง
@@ -94,8 +82,12 @@ public class MainFrame extends JFrame{
         JLabel smallImageLabel = new JLabel(scaledIcon);
         JPanel2.add(smallImageLabel);
 
-        // สร้าง JLabel สำหรับข้อความ
+        // สร้าง JLabel สำหรับข้อความ BorBorAndTheGang
         JLabel textLabel = new JLabel("BorBor And The Gang");
+        Font originalFont = textLabel.getFont();
+        int newSize = 16;
+        Font newFont = originalFont.deriveFont((float) newSize);
+        textLabel.setFont(newFont);
         textLabel.setVerticalAlignment(JLabel.TOP);
         textLabel.setHorizontalAlignment(JLabel.LEFT);
         JPanel2.add(textLabel);
@@ -103,15 +95,115 @@ public class MainFrame extends JFrame{
         // สร้าง ImageIcon สำหรับรูปภาพลำโพง
         ImageIcon additionalIcon = new ImageIcon("src/picture/speaker.png");
         Image additionalImage = additionalIcon.getImage();
-        //ปรับขนาดรูป
-        int additionalWidth = 20;
-        int additionalHeight = 20;
+        int additionalWidth = 30;
+        int additionalHeight = 30;
         Image scaledAdditionalImage = additionalImage.getScaledInstance(additionalWidth, additionalHeight, Image.SCALE_SMOOTH);
         ImageIcon scaledAdditionalIcon = new ImageIcon(scaledAdditionalImage);
         JLabel additionalImageLabel = new JLabel(scaledAdditionalIcon);
         JPanel2.add(additionalImageLabel);
+
         JPanel2.setLayout(new FlowLayout(FlowLayout.LEFT));
         JPanelCenter.add(JPanel2);
+
+        //เพิ่ม text TH/EN
+        JLabel textLabel1 = new JLabel("TH/EN");
+        textLabel1.setVerticalAlignment(JLabel.TOP);
+        textLabel1.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel1);
+
+
+        // สร้าง ImageIcon สำหรับรูปภาพนาริกา
+        ImageIcon newIcon = new ImageIcon("src/picture/Clock.png");
+        Image newImage = newIcon.getImage();
+        int newWidth = 30;
+        int newHeight = 30;
+        Image scaledNewImage = newImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        ImageIcon scaledNewIcon = new ImageIcon(scaledNewImage);
+        JLabel newImageLabel = new JLabel(scaledNewIcon);
+        JPanel2.add(newImageLabel);
+
+        //เพิ่ม text 120 MINS
+        JLabel textLabel2 = new JLabel("120 MINS");
+        textLabel2.setVerticalAlignment(JLabel.TOP);
+        textLabel2.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel2);
+
+        // สร้าง ImageIcon สำหรับรูปภาพสองดี
+        ImageIcon addIcon = new ImageIcon("src/picture/2D.png");
+        Image addImage = addIcon.getImage();
+        int addWidth = 40;
+        int addHeight = 25;
+        Image scaledAddImage = addImage.getScaledInstance(addWidth, addHeight, Image.SCALE_SMOOTH);
+        ImageIcon scaledAddIcon = new ImageIcon(scaledAddImage);
+        JLabel addImageLabel = new JLabel(scaledAddIcon);
+        JPanel2.add(addImageLabel);
+
+        //เพิ่ม text showtime
+        JLabel textLabel3 = new JLabel("ShowTime");
+        textLabel3.setVerticalAlignment(JLabel.TOP);
+        textLabel3.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel3);
+
+        //เพิ่ม text swu
+        JLabel textLabel4 = new JLabel("Srinakharinwirot Cinema");
+        textLabel4.setVerticalAlignment(JLabel.TOP);
+        textLabel4.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel4);
+
+        //เพิ่ม text date
+        JLabel textLabel5 = new JLabel("MON 13 NOV");
+        textLabel5.setVerticalAlignment(JLabel.TOP);
+        textLabel5.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel5);
+
+
+
+
+        //เพิ่ม text โรงหนัง
+        JLabel textLabel6 = new JLabel("Theater7");
+        textLabel6.setVerticalAlignment(JLabel.TOP);
+        textLabel6.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel6);
+
+        // สร้าง ImageIcon สำหรับรูปภาพเวลา
+        ImageIcon add1Icon = new ImageIcon("src/picture/Time.png");
+        Image add1Image = add1Icon.getImage();
+        int add1Width = 50;
+        int add1Height = 35;
+        Image scaledAdd1Image = add1Image.getScaledInstance(add1Width, add1Height, Image.SCALE_SMOOTH);
+        ImageIcon scaledAdd1Icon = new ImageIcon(scaledAdd1Image);
+        JLabel add1ImageLabel = new JLabel(scaledAdd1Icon);
+        JPanel2.add(add1ImageLabel);
+
+        //เพิ่ม text ที่นั่ง
+        JLabel textLabel7 = new JLabel("SEAT: ");
+        textLabel7.setVerticalAlignment(JLabel.TOP);
+        textLabel7.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel7);
+
+        //เพิ่ม text ราคา
+        JLabel textLabel8 = new JLabel("TOTAL PRICE");
+        textLabel8.setVerticalAlignment(JLabel.TOP);
+        textLabel8.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel8);
+
+        //เพิ่ม text เลขที่นั่ง
+        JLabel textLabel9 = new JLabel("E6");
+        Font originalFont9 = textLabel9.getFont();
+        int newSize9 = 16;
+        Font newFont9 = originalFont9.deriveFont((float) newSize9);
+        textLabel9.setFont(newFont9);
+        textLabel9.setVerticalAlignment(JLabel.TOP);
+        textLabel9.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel9);
+
+
+        //เพิ่ม text ราคารวม
+        JLabel textLabel10 = new JLabel("160 BATH");
+        textLabel10.setVerticalAlignment(JLabel.TOP);
+        textLabel10.setHorizontalAlignment(JLabel.LEFT);
+        JPanel2.add(textLabel10);
+
 
 
         JPanelCenter.setLayout(new FlowLayout(FlowLayout.CENTER));
